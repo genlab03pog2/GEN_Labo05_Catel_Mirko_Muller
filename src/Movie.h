16f6b7527a9b01adbdf6aeb5bb36112ae04b3426
Ichimizu <arzur.cateltorres@heig-vd.ch>
2019-06-16 19:04:27 +0200

    methode setPriceType ajoutee

--- src/Movie.h
+++ src/Movie.h
@@ -10,6 +10,7 @@ class Movie {
 public:
   Movie(const std::string& title, std::shared_ptr<Price> price);
   std::string getTitle() const;
+  void setPriceType(std::shared_ptr<Price> price);
   std::shared_ptr<Price> getPrice() const;
 
 private:
@@ -20,6 +21,8 @@ private:
 inline Movie::Movie(const std::string& title, std::shared_ptr<Price> price) : _title( title ), price( price ){}
 
 inline std::string Movie::getTitle() const { return _title; }
+
+inline void Movie::setPriceType(std::shared_ptr<Price> price) {this->price = price; }
 
 inline std::shared_ptr<Price> Movie::getPrice() const { return price;}
 
---
+++ test/RentalTest.cpp
@@ -0,0 +1,9 @@
+#include "../src/Customer.h"
+#include "../src/PriceRegular.h"
+#include "../src/PriceNewRealease.h"
+#include "../src/PriceChildren.h"
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <iostream>
+
