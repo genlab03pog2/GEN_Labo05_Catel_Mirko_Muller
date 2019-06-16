7a9d4a537d4add601549ba4fabdb81876fdc2838
Pierrick Muller <pierrick.muller@heig-vd.ch>
2019-06-16 18:48:56 +0200

    Ajout tests customerGetName et testStatementSimpleAddOnOneCustomer

--- test/CustomerTests.cpp
+++ test/CustomerTests.cpp
@@ -8,15 +8,30 @@
 #include "../src/PriceNewRealease.h"
 #include "../src/PriceChildren.h"
 
-TEST(customer_Test, statement)
-{
-
-Customer customer("Customer");
-customer.addRental( Rental( Movie("film1", std::shared_ptr<Price>(new PriceRegular())), 1));
-customer.addRental( Rental( Movie( "film2", std::shared_ptr<Price>(new PriceNewRelease())), 2));
-customer.addRental( Rental( Movie("film3", std::shared_ptr<Price>(new PriceChildren())), 3 ));
-
-std::string expected = "Rental Record for Customer\n\tfilm1\t2\n\tfilm2\t6\n\tfilm3\t1.5\nAmount owed is 9.5\nYou earned 4 frequent renter points";
-
-EXPECT_EQ(customer.statement(),expected);
+TEST(customer_Test, customerGetName){
+    Customer customer("Customer");
+    EXPECT_EQ(customer.getName(),"Customer");
 }
+
+TEST(customer_Test, testStatementMultipleAddOnOneCustomer)
+{
+    Customer customer("Customer");
+    customer.addRental( Rental( Movie("film1", std::shared_ptr<Price>(new PriceRegular())), 1));
+    customer.addRental( Rental( Movie( "film2", std::shared_ptr<Price>(new PriceNewRelease())), 2));
+    customer.addRental( Rental( Movie("film3", std::shared_ptr<Price>(new PriceChildren())), 3 ));
+
+    std::string expected = "Rental Record for Customer\n\tfilm1\t2\n\tfilm2\t6\n\tfilm3\t1.5\nAmount owed is 9.5\nYou earned 4 frequent renter points";
+
+    EXPECT_EQ(customer.statement(),expected);
+}
+
+TEST(customer_test, testStatementSimpleAddOnOneCustomer)
+{
+    Customer customer("Customer");
+    customer.addRental( Rental ( Movie( "film1",std::shared_ptr<Price>(new PriceRegular())),1));
+
+    std::string expected = "Rental Record for Customer\n\tfilm1\t2\nAmount owed is 2\nYou earned 1 frequent renter points";
+    EXPECT_EQ(customer.statement(),expected);
+}
+
+
